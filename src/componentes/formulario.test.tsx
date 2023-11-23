a41a1e4e2ecf3d095dbfec4cc915b367682c6263
Mackenzzy <mackenzzy@gmail.com>
2023-11-23 18:32:45 -0300

    Formulario criado

--- src/componentes/formulario.test.tsx
+++ src/componentes/formulario.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import Formulario from "./Formulario";
 
 //Jest 
 
@@ -16,8 +17,8 @@ test('Quando o input estiver vazio, novos participantes não podem ser adiciona
     //esperar q o input esteja no documento
     expect(input).toBeInTheDocument()
 
-    //esperar q o botão esteja no documento
-    expect(botao).toBeInTheDocument()
-}
+    //esperar q o botão esteja desabilitado
+    expect(botao).toBeDisabled()
+} 
 
 )
